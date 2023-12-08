33c0c01adde12d508d90bcaa43eac95c248598a2
rodroadl <kkgg3781@gmail.com>
2023-12-07 23:14:33 -0500

    seperated from training phase and utilize fold

--- CCCNN/eval.py
+++ CCCNN/eval.py
@@ -2,7 +2,7 @@
 eval.py
 
 Last edited by: GunGyeom James Kim
-Last edited at: Dec 5th, 2023
+Last edited at: Dec 7th, 2023
 
 Evaluate the CCCNN
 '''
@@ -10,6 +10,7 @@ import os
 import argparse
 import cv2
 import matplotlib.pyplot as plt
+import tqdm
 
 # torch
 import torch
@@ -25,71 +26,78 @@ def main():
     '''
     Driver function to test the network
     '''
-    # initialize the argument parser
-    args = get_args()
-    evaluate(args)
+    ## initialize the argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log-space', default=False, action='stroe_true')
+    parser.add_argument('--num-patches', type=int, required=True)
+    parser.add_argument('--fold-file', type=str, required=True)
+    parser.add_argument('--weights-file', type=str, required=True)
+    parser.add_argument('--images-dir', type=str, required=True)
+    parser.add_argument('--labels-file', type=str, required=True)
+    parser.add_argument('--outputs-dir', type=str, required=True)
+    parser.add_argument('--num-workers', type=int, default=os.cpu_count())
+    args = parser.parse_args()
 
-def evaluate(args):
-    if not os.path.exists(args.outputs_dir): os.makedirs(args.outputs_dir)
-
-    # set up device and initialize the network
+    ## set up device and initialize the network
     cudnn.benchmark = True
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = CCCNN().to(device)
+    pred_dir = os.path.join(args.outputs_dir, '{}'.format(args.weights_file[:-4]))
+
+    if not os.path.exists(pred_dir): os.makedirs(pred_dir)
+
     state_dict = model.state_dict()
 
-    # load the saved parameters
-    for n, p in torch.load(args.weights_file, map_location= lambda storage, loc: storage).items():
+    pth_path = os.path.join(args.outputs_dir, "pth/{}".format(args.weights_file))
+    _, _, fold_test = open(args.fold_file, "r")
+    fold_test = list(map(int, fold_test.split(",")))
+
+    ## load the saved parameters
+    for n, p in torch.load(pth_path, map_location= lambda storage, loc: storage).items():
         if n in state_dict.keys(): state_dict[n].copy_(p)
         else: raise KeyError(n)
+
     model.eval()
 
-    # configure datasets and dataloaders
-    ref_dataset = ReferenceDataset(args.images_dir, args.labels_file)
-    eval_dataset = CustomDataset(args.images_dir, args.labels_file, num_patches=args.num_patches, image_space=args.image_space, label_space=args.label_space)
-    eval_dataloader = DataLoader(dataset=eval_dataset, 
+    ## configure datasets and dataloaders
+    test_dataset = CustomDataset(args.images_dir, args.labels_file, fold_test, num_patches=args.num_patches, log_space=args.log_space, seed=args.seed)
+    ref_dataset = ReferenceDataset(args.images_dir, args.labels_file, fold_test)
+    test_dataloader = DataLoader(dataset=test_dataset, 
                                 batch_size=1,
                                 num_workers=args.num_workers
                                 )
     
     losses = []
-    for  (batch, data) in zip(eval_dataloader, ref_dataset):
-        input, label, name = data
-        inputs, labels = batch
-        inputs = torch.flatten(inputs, start_dim=0, end_dim=1) #[batch size, num_patches, ...] -> [batch size * num_patches, ...] / NOTE: optimize?
-        labels = torch.flatten(labels, start_dim=0, end_dim=1)
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        input = input.to(device)
-        label = label.to(device)
+    with tqdm(total=(len(ref_dataset))) as test_pbar:
+        test_pbar.set_description('test progression:')
 
-        with torch.no_grad(): preds = model(inputs)
+        for  (test_batch, data) in zip(test_dataloader, ref_dataset):
+            input, label, name = data
+            inputs, labels = test_batch
+            inputs = torch.flatten(inputs, start_dim=0, end_dim=1) #[batch size, num_patches, ...] -> [batch size * num_patches, ...] / NOTE: optimize?
+            labels = torch.flatten(labels, start_dim=0, end_dim=1)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            input = input.to(device)
+            label = label.to(device)
 
-        if args.label_space == "log": # [-infty, 0)
-            eps = 1e-7
-            preds = torch.exp(label-eps)
-            if torch.isnan(preds).any():
-                print("nan in preds")
-                raise SystemExit
-        elif args.label_space == "expandedLog":
-            preds = torch.where(preds != 0, torch.exp(preds), 0.)
-        
-        # map to rgb chromaticty space
-        preds = to_rgb(preds)
+            with torch.no_grad(): preds = model(inputs)
+            
+            ### map to rgb chromaticty space
+            mean_pred = torch.mean(preds, dim=0)
 
-        mean_pred = torch.mean(preds, dim=0)
-        # print("mean_pred:", mean_pred)
-        # print("label:", label)
-        loss = angularLoss(mean_pred, label, singleton=True)
-        losses.append(loss)
+            loss = angularLoss(mean_pred, label, singleton=True)
+            losses.append(loss)
 
-        # reconstruct PNG to JPG with gt/pred illumination
-        pred_img = illuminate(input, mean_pred)
+            ### reconstruct PNG to JPG with gt/pred illumination
+            black_lvl = 129 if name[:3] == "IMG" else 0 # GehlerShi
+            pred_img = illuminate(input, mean_pred, black_lvl)
+            
+            ### save the reconstructed image
+            cv2.imwrite(os.path.join(pred_dir,'pred_{}.jpg'.format(name[:-4])), cv2.cvtColor(pred_img, cv2.COLOR_RGB2BGR))
+            test_pbar.update(1)
 
-        # save the reconstructed image
-        cv2.imwrite(os.path.join(args.outputs_dir,'pred_{}.jpg'.format(name)), cv2.cvtColor(pred_img, cv2.COLOR_RGB2BGR))
-
-    # calculate stats
+    ### calculate stats
     losses.sort()
     l = len(losses)
     minimum = min(losses)
@@ -101,23 +109,9 @@ def evaluate(args):
 
     print("Min: {}\n10th per: {}\nMed: {}\nAvg: {}\n 90th per: {}\nMax: {}\n".format(minimum, tenth, median, average, ninetieth, maximum))
 
-    # draw histogram
+    ### draw histogram
     plt.hist(losses)
     plt.show()
-
-def get_args():
-    # initialize the argument parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--image-space', type=str, default='linear')
-    parser.add_argument('--label-space', type=str, default='linear')
-    parser.add_argument('--num-patches', type=int, required=True)
-    parser.add_argument('--weights-file', type=str, required=True)
-    parser.add_argument('--images-dir', type=str, required=True)
-    parser.add_argument('--labels-file', type=str, required=True)
-    parser.add_argument('--outputs-dir', type=str, required=True)
-    parser.add_argument('--num-workers', type=int, default=os.cpu_count())
-    args = parser.parse_args()
-    return args
 
 if __name__ == '__main__':
     main()
